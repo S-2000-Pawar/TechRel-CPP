6a4af7d939a567977970473d6daa32ea5797acae
S-2000-Pawar <85825283+S-2000-Pawar@users.noreply.github.com>
2021-06-20 16:16:55 +0530

    Add files via upload
    Array,Memory address,Pointer programs

---
+++ array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string cars[4]={"Volvo","BMW","Ford","Mazda"};
+	cars[2]="Opel";
+	cout<<cars[0]<<endl;
+	cout<<cars[2]<<endl;
+	return 0;
+}
---
+++ pointer.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string food="Pizza";
+	string *ptr=&food;
+	cout<<food<<endl;
+	cout<<&food<<endl;
+	cout<<ptr<<endl;
+	cout<<*ptr<<endl;
+	*ptr="Hamburger";
+	cout<<*ptr<<endl;
+	cout<<food<<endl;
+	return 0;
+}
